5f2fead80c713571d4191d4065c7d6cd28d6a418
Rafael Grande <rafaelgrande@outlook.com>
2017-12-26 17:41:48 -0200

    Inserindo plugin babel es2015 e criando configuração do webpack na versão 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,43 @@
 const path = require('path');
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'docs')
-  }
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'docs')
+    },
+    plugins: [
+        new ExtractTextPlugin('app.css'),
+    ],
+    devServer: {
+        contentBase: './docs'
+    },
+    module: {
+         rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.js[x]?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        babelrc: false,
+                        presets: [
+                            ['es2015', { modules: false }],
+                            'react',
+                        ],
+                        plugins: ['transform-object-rest-spread']
+                    }
+                }
+            }
+        ]
+    }
 };
